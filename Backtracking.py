efd49129c65a69bf92d31907acf8bb20c1b55050
Aka-Ayaan <am08853@st.habib.edu.pk>
2024-03-30 14:30:53 +0500

    done

---
+++ Backtracking.py
@@ -0,0 +1,48 @@
+puzzle = [
+            [5, 3, 0, 0, 7, 0, 0, 0, 0],
+            [6, 0, 0, 1, 9, 5, 0, 0, 0],
+            [0, 9, 8, 0, 0, 0, 0, 6, 0],
+            [8, 0, 0, 0, 6, 0, 0, 0, 3],
+            [4, 0, 0, 8, 0, 3, 0, 0, 1],
+            [7, 0, 0, 0, 2, 0, 0, 0, 6],
+            [0, 6, 0, 0, 0, 0, 2, 8, 0],
+            [0, 0, 0, 4, 1, 9, 0, 0, 5],
+            [0, 0, 0, 0, 8, 0, 0, 7, 9]
+        ]
+
+def solve_sudoku(board):
+    empty = find_empty(board)
+    if not empty:
+        return True
+
+    row, col = empty
+
+    for num in range(1, 10):
+        if is_valid(board, row, col, num):
+            board[row][col] = num
+
+            if solve_sudoku(board):
+                return True
+
+            board[row][col] = 0
+
+    return False
+
+def find_empty(board):
+    for i in range(9):
+        for j in range(9):
+            if board[i][j] == 0:
+                return (i, j)
+    return None
+
+def is_valid(board, row, col, num):
+    # Check if the number is not in the row, column, and 3x3 box
+    return (
+        all(num != board[row][j] for j in range(9)) and
+        all(num != board[i][col] for i in range(9)) and
+        all(num != board[i][j] for i in range(row // 3 * 3, row // 3 * 3 + 3) for j in range(col // 3 * 3, col // 3 * 3 + 3))
+    )
+
+print(solve_sudoku(puzzle))
+for row in puzzle:
+    print(row)
